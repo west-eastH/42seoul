b76c96f2cc69a4e8b3d66105dc861f21a940bd83
Donghyeon Seo <dongseo@c1r4s1.42seoul.kr>
2023-08-03 13:36:55 +0900

    2

---
+++ sample/ft_printf/ft_printf.h
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dongseo <dongseo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/17 19:12:41 by dongseo           #+#    #+#             */
+/*   Updated: 2023/06/20 14:10:29 by dongseo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+
+# include <stdarg.h>
+# include <unistd.h>
+
+size_t				ft_strlen(const char *s);
+ssize_t				st_len(const char *st);
+
+void				ft_putnbr_di(int n, ssize_t *len);
+void				ft_putnbr_u(unsigned int u, ssize_t *len);
+void				ft_putnbr_x(unsigned int x, ssize_t *len, int is_upper);
+unsigned long long	set_base(int base[], unsigned long long p);
+
+ssize_t				ft_printf_c(char c);
+ssize_t				ft_printf_s(char *s);
+ssize_t				ft_printf_di(int d);
+ssize_t				ft_printf_u(unsigned int u);
+ssize_t				ft_printf_x(int x, int is_upper);
+ssize_t				ft_printf_p(unsigned long long p);
+
+ssize_t				check_format(char format, va_list *ap);
+int					ft_printf(const char *st, ...);
+#endif
---
+++ sample/sample.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sample.h                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dongseo <dongseo@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/28 14:52:57 by dongseo           #+#    #+#             */
+/*   Updated: 2023/08/03 13:34:32 by dongseo          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SAMPLE_H
+# define SAMPLE_H
+
+# include "ft_printf/ft_printf.h"
+# include "get_next_line/get_next_line.h"
+# include "libft/libft.h"
+#endif
