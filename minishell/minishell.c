301b7d4cecb8146ecb0bb0ebaf303d1f53ac66e4
Donghyeon Seo <dongseo@c1r5s6.42seoul.kr>
2023-08-12 10:43:08 +0900

    1

--- minishell/minishell.c
+++ minishell/minishell.c
@@ -1,41 +1,21 @@
-#include <signal.h>
 #include <stdio.h>
-#include <readline/readline.h>
-#include <readline/history.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <signal.h>
 
-void handler(int signum)
+void (*old_fun)( int);
+
+void sigint_handler( int signo)
 {
-    if (signum != SIGINT)
-        return;
-    printf("ctrl + c\n");
-    rl_on_new_line();
-    rl_replace_line("", 1);
-    rl_redisplay();
+	(void)signo;
+   printf( "Ctrl-C 키를 눌루셨죠!!\n");
+   printf( " 종료됩니다.\n");
 }
 
-int main(void)
+int main( void)
 {
-    int ret;
-    char *line;
-
-    signal(SIGINT, handler);
-    while (1)
-    {
-        line = readline("input> ");
-        if (line)
-        {
-            if (ret)
-                printf("output> %s\n", line);
-            add_history(line);
-            free(line);
-            line = NULL;
-        }
-        else
-        {
-            printf("ctrl + d\n");
-        }
-    }
-    return (0);
+   old_fun = signal( SIGINT, sigint_handler);
+   while(1 ){
+      printf( "badayak.com\n");
+      sleep( 1);
+   }
 }
