d3090bdb5fb8913abe5698c20345a3bcf7d7cdf6
Donghyeon Seo <dongseo@c1r9s5.42seoul.kr>
2024-02-20 15:49:42 +0900

    [feat] : cpp09

---
+++ cpp09/ex01/RPN.cpp
@@ -0,0 +1,93 @@
+#include "RPN.hpp"
+
+RPN::RPN(char* exp) : _exp(exp) {}
+
+RPN::~RPN(){}
+
+bool RPN::isValidExpression()
+{                       
+	std::string oper = "+-/*";
+	bool flag = false;
+	for (size_t i = 0; i < _exp.length(); i++)
+	{
+		if (_exp[i] == ' ')
+		{
+			if (flag == false || i == _exp.length() - 1)
+				return false;
+			flag = false;
+			continue ;
+		}
+		if (_exp[i] < '0' || _exp[i] > '9')
+		{
+			if (oper.find(_exp[i]) == std::string::npos)
+				return false;
+		}
+		if (flag == true)
+			return false;
+		flag = true;
+		_vec.push_back(_exp[i]);
+	}
+	return true;
+}
+
+bool RPN::checkStack(char op)
+{
+	if (_stack.size() < 2)
+	{
+		std::cout << "Error" << std::endl;
+		return true;
+	}
+	int b = _stack.top();
+	_stack.pop();
+	int a = _stack.top();
+	_stack.pop();
+	switch (op)
+	{
+		case '+' :
+			_stack.push(a + b);
+			break ;
+		case '-' :
+			_stack.push(a - b);
+			break ;
+		case '/' :
+			_stack.push(a / b);
+			break ;
+		case '*' :
+			_stack.push(a * b);
+			break ;
+		default:
+			std::cout << "Error" << std::endl;
+			return true;
+	}
+	return false;
+}
+
+void RPN::calculate()
+{
+	for (size_t i = 0; i < _vec.size(); i++)
+	{
+		if (_vec[i] >= '0' && _vec[i] <= '9')
+		{
+			_stack.push(_vec[i] - '0');
+			continue;
+		}
+		if (checkStack(_vec[i]))
+			return ;
+	}
+	if (_stack.size() != 1)
+	{
+		std::cout << "Error" << std::endl;
+		return ;
+	}
+	std::cout << _stack.top() << std::endl;
+}
+
+void RPN::start()
+{
+	if (!isValidExpression())
+	{
+		std::cout << "Error" << std::endl;
+		return ;
+	}
+	calculate();
+}
---
+++ cpp09/ex01/RPN.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <iostream>
+#include <stack>
+#include <vector>
+
+class RPN
+{
+	private:
+		std::string _exp;
+		std::vector<char> _vec;
+		std::stack<int> _stack;
+		RPN();
+		RPN(const RPN& origin);
+		RPN& operator=(const RPN& origin);
+	public:
+		RPN(char* exp);
+		~RPN();
+		void start();
+		bool isValidExpression();
+		bool checkStack(char op);
+		void calculate();
+};
